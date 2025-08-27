7583186a09b8e74bc066ed5cca768b428c93e7b0
YbrDev-web <ybenromdhane2003@gmail.com>
2025-08-27 12:10:03 +0200

    modification des fichiers

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
 // Point d'entrée unique (Front Controller)
+
+// Démarrer la session (important pour auth)
+//session_start();
+
+// Charger le router
 require_once __DIR__ . '/../app/core/Router.php';
 
+// Récupérer le paramètre "url" (redirigé par Nginx ou Apache)
+$url = $_GET['url'] ?? '';
+
+// Router
 $router = new Router();
-$router->handleRequest($_GET['url'] ?? '');
+$router->handleRequest($url);
+
